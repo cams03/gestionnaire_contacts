72dd26742f2196b30c8768d38a2a3823ee5e729a
Cams03 <camille.francia@hotmail.fr>
2017-01-24 14:34:26 +0100

    delete option added

--- js/contacts.js
+++ js/contacts.js
@@ -1,16 +1,10 @@
-/* 
-Activité : gestion des contacts
-*/
+var Contacts = {
 
-// TODO : complétez le programme
-
-var Contacts = { 
-    
     init: function (nom, prenom) {
         this.nom = nom;
         this.prenom = prenom;
     },
-    
+
     decrire: function () {
         var description = "Nom : " + this.nom + ", Prénom : " + this.prenom;
         return description;
@@ -30,23 +24,24 @@ table.push(personne2);
 console.log("Bienvenue dans le gestionnaire des contacts !");
 
 var saisieMenu = 1;
+var i = 0;
 
 while (saisieMenu !== 0) {
-    
-    console.log("1 : Lister les contacts \n2 : Ajouter un contact \n0 : Quitter");
-    
+
+    console.log("1 : Lister les contacts \n2 : Ajouter un contact \n3 : Supprimer le dernier contact \n0 : Quitter");
+
     saisieMenu = Number(prompt("Choisissez une option", "<Entrez une option>"));
-    
-    if ((saisieMenu !== 0) && (saisieMenu !==1) && (saisieMenu !== 2)) {
-        console.log("Avez-vous fait une erreur? Les choix possibles sont 1, 2 ou 0. Entrez de nouveau une option");
+
+    if ((saisieMenu !== 0) && (saisieMenu !== 1) && (saisieMenu !== 2) && (saisieMenu !== 3)) {
+        console.log("Avez-vous fait une erreur? Les choix possibles sont 1, 2, 3 ou 0. Entrez de nouveau une option");
     }
-        
+
     if (saisieMenu === 1) {
         console.log("Voici la liste de vos contacts :");
         table.forEach(function (personne) {
-        console.log(personne.decrire());
+            console.log(personne.decrire());
         });
-        
+
     } else if (saisieMenu === 2) {
         nom = prompt("Entrez un nom :", "<Entrez un nom>");
         prenom = prompt("Entrez un prénom :", "<Entrez un prénom>");
@@ -54,9 +49,12 @@ while (saisieMenu !== 0) {
         newContact.init(nom, prenom);
         table.push(newContact);
         console.log("Le nouveau contact a été ajouté");
-        
+
+    } else if (saisieMenu === 3) {
+        table.pop();
+        console.log("Le dernier contact a été supprimé");
+
     } else if (saisieMenu === 0) {
         console.log("Au revoir !");
-    } 
+    }
 };
-
